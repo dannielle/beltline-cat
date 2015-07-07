a939872c8fa03bf0650aaaf39a76df0274697b27
dannielle <dannielle.del@gmail.com>
2015-07-06 23:26:38 -0400

    Initial cat commit! Basic movement and win state

---
+++ Assets/Scripts/CatCollideScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatCollideScript : MonoBehaviour {
+
+	SpriteRenderer sr;
+
+	public Sprite winSprite;
+
+	bool playerIsAdjacent;
+	
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+		playerIsAdjacent = false;
+	}
+	
+	void Update () {
+		if ( ( Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.Space) ) && playerIsAdjacent) {
+			sr.sprite = winSprite;
+		}
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D otherCollider) {
+		//is this a cat?
+		string cat = otherCollider.gameObject.name;
+		if (cat.Equals("player")) {
+			playerIsAdjacent = true;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	private Vector2 movement;
+
+	public Sprite walkingSprite;
+	public Sprite creepingSprite;
+	public Sprite crouchingSprite;
+
+	SpriteRenderer sr;
+	
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+	}
+
+	public enum State {
+		Walk,
+		Creep,
+		Crouch
+	}
+
+	public State state;
+	
+	void Update () {
+		float inputX = Input.GetAxis ("Horizontal");
+		movement = new Vector2 (inputX, 0);
+
+
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.LeftArrow )) {
+			state = State.Walk;
+		}
+
+		else if (Input.GetKey (KeyCode.Space) || Input.GetKey (KeyCode.DownArrow)) {
+			state = State.Crouch;
+		}
+
+		else {
+			state = State.Creep;
+		}
+
+		switch (state) {
+		case State.Walk:
+			sr.sprite = walkingSprite;
+			break;
+		case State.Creep:
+			sr.sprite = creepingSprite;
+			break;
+		case State.Crouch:
+			sr.sprite = crouchingSprite;
+			break;
+		default:
+			sr.sprite = creepingSprite;
+			break;
+		}
+	}
+
+	void FixedUpdate () {
+		rigidbody2D.velocity = movement;
+	}
+}
